20421c8c678f644b763bc89d30f84b0bb816bba3
christianmaierger <c.maier1122@gmail.com>
2021-11-09 20:48:57 +0100

    add a dynamodb  service with basic IAM to serverless.yml todo will be a better key schema and also refining of function to get items from db table

--- handler.js
+++ handler.js
@@ -3,6 +3,9 @@
 var AWS = require('aws-sdk');
 // Create DynamoDB document client
 var docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+// get our reference to table from environment variables
+const TableName = process.env.TABLE_NAME
+
 
 module.exports.hello = async(event) => {
     return {
@@ -20,21 +23,38 @@ module.exports.hello = async(event) => {
 
 module.exports.getItemFromDB = async(event) => {
 
-    let id;
-    // if no id was given, or null or no event in general was recieved or another format
-    // than a number was given 400 is returned as this is most likely a client side error
-    if (!event || event.key1 === undefined || event.key1 === null || !(typeof event.key1 === "number")) {
+    let anyTypeID;
+    let typeOfID;
+    let integerID;
+    if (!event || event.key1 === undefined || event.key1 === null || isNaN(event.key1) || isNaN(parseFloat(event.key1))) {
         const response = {
             statusCode: 400,
         };
         return response;
-    } else {
-        id = event.key1;
+    }
+    typeOfID = typeof event.key1;
+    anyTypeID = event.key1;
+
+    if (typeof event.key1 === "number") {
+        integerID = anyTypeID;
+    }
+    if ((typeOfID === 'string')) {
+        if (!isNaN(anyTypeID) && !isNaN(parseFloat(anyTypeID))) {
+            integerID = parseInt(anyTypeID, 10)
+        } else {
+            const response = {
+                statusCode: 400,
+            };
+            return response;
+        }
     }
 
     var params = {
-        TableName: 'MyTable',
-        Key: { 'ID': id }
+        TableName: TableName,
+        Key: {
+            'ID': integerID,
+            'Name': event.key2
+        }
     };
 
     let result = await docClient.get(params, function(err, data) {
@@ -49,7 +69,7 @@ module.exports.getItemFromDB = async(event) => {
         } else {
             // if item was found it is in data.Item, otherwise data.Item is empty
             if (data.Item) {
-                console.log("data should have a value: " + data);
+                console.log("data should have a value: " + data.Item);
                 return data;
             } else {
                 console.log("data should be empty " + data);
@@ -59,7 +79,6 @@ module.exports.getItemFromDB = async(event) => {
         }
     }).promise();
 
-
     let i = 0;
     for (let attr in result.Item) {
         console.log("Attr" + i++ + "= " + attr);
@@ -67,7 +86,7 @@ module.exports.getItemFromDB = async(event) => {
     console.log("Result is " + result);
     console.log("Result.Item is " + result.Item);
 
-    if (result.item === undefined) {
+    if (result.Item === undefined) {
         const response = {
             statusCode: 404,
         };
@@ -90,13 +109,12 @@ module.exports.writeToDB = async(event) => {
     let res = this.getItemFromDB();
 
 
-
     var params = {
-        TableName: 'MyTable',
+        TableName: TableName,
         Item: {
-            'Key': VALUE,
-            'ATTRIBUTE_1': 'STRING_VALUE',
-            'ATTRIBUTE_2': VALUE_2
+            'ID': event.body.key1,
+            'Name': event.body.key2,
+            'Surname': event.body.Key3,
         }
     };
 
@@ -107,8 +125,6 @@ module.exports.writeToDB = async(event) => {
             console.log("Success", data);
         }
     });
-
-
 
     return {
         statusCode: 200,
