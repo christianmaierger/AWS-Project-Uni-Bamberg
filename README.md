bbee133e255f705a195d8650a30e5360a2429e16
Max Meidinger <max.meidinger99@gmail.com>
2021-11-24 15:42:08 +0100

    partial refactor done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# group1
+## Weird Table definition interaction
+When defining a table as a resource in the `serverless.yml` file, there a two names to be given:
+- The yaml - keyname directly in the Resources list
+- The Tablename defined by `TableName` inside the resource definition
 
+Those two names do not need to be the same, the first one however servers as a unique identifier for the table, which is not visible in the dynamoDB webinterface.
+
+ With the second one, you define the displayed name of the table in the webinterface.
+
+ This can lead to some weird interactions, like only renaming your table if you only change the second name, or not being able to create another table with the same first name.
+
+ EDIT: Even when changing both names, the database renamed/overwritten (?)
+ -> Maybe its bound to the specific project?
--- handler.js
+++ handler.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
 module.exports.hello = async (event) => {
-  var ddb = AWS.DynamoDB();
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -15,3 +16,65 @@ module.exports.hello = async (event) => {
     ),
   };
 };
+
+function wrapResponse(statusCode, data) {
+  return {
+    statusCode: statusCode,
+    body: data
+  }
+}
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+
+module.exports.echo = async (event) => {
+  return wrapResponse(200, event);
+}
+
+//TODO Autoincremtent: Get highest id from table and write new one (Threadsafety?)
+//OR just hash an id from name etc
+//Better way to get new id? extra function that can only run one instance at a time that gives new ids?
+//TODO Would it make sense to not have to adjust the adjust the data in event to "N" "S" etc before sending it to this function
+module.exports.put = async (event) => {
+
+  var params = {
+    TableName: process.env.TABLE_NAME,
+    Item: event
+  }
+
+  //TODO Query after item with given id, throw error if it exists -> race condition! (normal in dbs?)
+  try {
+    await docClient.put(params).promise();
+    return wrapResponse(200, { message:"Creation of entry successful"});
+  } catch (error) {
+    return wrapResponse(error.statusCode, { message: error.message});
+  }
+}
+
+module.exports.change = async (event) => {
+
+}
+
+module.exports.get = async (event) => {
+  var params = {
+    TableName: process.env.TABLE_NAME,
+    Key: event
+  }
+  
+  try {
+    let response = await docClient.get(params).promise();
+    if(response && isEmpty(response)) {
+      return wrapResponse(404, { message: "No entry matched the given parameters."});
+    }
+    return wrapResponse(200, response);
+  } catch (error) {
+    return wrapResponse(error.statusCode, { message: error.message});
+  }
+}
+
+module.exports.remove = async (event) => {
+
+}
+
