ef35d5f40db95a52ecad665eb4452b398b95cf60
Max Meidinger <max.meidinger99@gmail.com>
2021-11-25 14:53:29 +0100

    add testdata and commands, refactor and add delete and update

--- handler.js
+++ handler.js
@@ -11,9 +11,7 @@ const lambda = new AWS.Lambda({
 const TableName = process.env.Table_Name;
 const GetFunction = process.env.Get_Function;
 
-
 module.exports.logItemChanges = async (event) => {
-
     console.log("Trigger of LOGGER PULLED as an User was added or changed: ");
     console.log(event);
 };
@@ -35,19 +33,50 @@ function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
 
-function validateEmail(event) {
+function validateEmail(email) {
     var validationRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (typeof myVar === 'string' && event.email.match(validationRegex)) {
-        console.log("Valid email address!");
+    if (email && typeof email === 'string' && email.match(validationRegex)) {
         return true;
     } else {
-        console.log("Invalid email address!");
         return false;
     }
 }
 
-module.exports.getItemFromDB = async (event) => {
+
+module.exports.create = async (event) => {
+
+    if(!validateEmail(event.email)) {
+        return wrapResponse(400, {message: "Bad Request: Not a valid email-adress"});
+    }
+    try {
+       const response = await lambda.invoke({
+            FunctionName: GetFunction,
+            InvocationType: 'RequestResponse', // is default
+            Payload: JSON.stringify(event, null, 2) // pass params
+        }).promise();
+        let payload = JSON.parse(response.Payload);
+        if (payload.statusCode === 200) {
+            return wrapResponse(405, {message: "Entry with given ID already exists, please use update to overide an existing entry"});
+        }
+    } catch (error) {
+        return wrapResponse(400, {message: "There was a Problem checking the Database"});
+    }
+
+    let params = wrapParams("Item", event);
+    try {
+        await docClient.put(params).promise();
+        return wrapResponse(200, {message: "Creation of entry successful"});
+    } catch (error) {
+        return wrapResponse(error.statusCode, {message: error.message});
+    }
+};
+
+module.exports.read = async (event) => {
+
+    if(!validateEmail(event.email)) {
+        return wrapResponse(400, {message: "Bad Request: Not a valid email-adress"});
+    }
 
     const params = wrapParams("Key", { email: event.email, name: event.name });
 
@@ -62,12 +91,9 @@ module.exports.getItemFromDB = async (event) => {
     }
 };
 
+module.exports.update = async (event) => {
 
-module.exports.create = async (event) => {
-    console.log(GetFunction)
-    console.log(TableName)
-
-    if(!validateEmail(event)) {
+    if(!validateEmail(event.email)) {
         return wrapResponse(400, {message: "Bad Request: Not a valid email-adress"});
     }
     // check if an item can be found under given id
@@ -78,9 +104,9 @@ module.exports.create = async (event) => {
             Payload: JSON.stringify(event, null, 2) // pass params
         }).promise();
         let payload = JSON.parse(response.Payload);
-        console.log(payload)
-        if (payload.statusCode === 200) {
-            return wrapResponse(405, {message: "Entry with given ID already exists, please use update to ovveride"});
+        if (payload.statusCode === 404) {
+            //No Item found -> cant update it
+            return wrapResponse(404, {message: "There is no entry to be updated"});
         }
     } catch (error) {
         return wrapResponse(400, {message: "There was a Problem checking the Database"});
@@ -89,8 +115,38 @@ module.exports.create = async (event) => {
     let params = wrapParams("Item", event);
     try {
         await docClient.put(params).promise();
-        return wrapResponse(200, {message: "Creation of entry successful"});
+        return wrapResponse(200, {message: "Entry updated successfully"});
     } catch (error) {
         return wrapResponse(error.statusCode, {message: error.message});
     }
+};
+
+module.exports.delete = async (event) => {
+
+    if(!validateEmail(event.email)) {
+        return wrapResponse(400, {message: "Bad Request: Not a valid email-adress"});
+    }
+
+    try {
+        const response = await lambda.invoke({
+             FunctionName: GetFunction,
+             InvocationType: 'RequestResponse', // is default
+             Payload: JSON.stringify(event, null, 2) // pass params
+         }).promise();
+         let payload = JSON.parse(response.Payload);
+         if (payload.statusCode === 404) {
+             //No Item found -> cant delete it
+             return wrapResponse(404, {message: "There is no entry to be deleted"});
+         }
+     } catch (error) {
+         return wrapResponse(400, {message: "There was a Problem checking the Database"});
+     }
+ 
+     let params = wrapParams("Key", event);
+     try {
+         await docClient.delete(params).promise();
+         return wrapResponse(200, {message: "Entry deleted successfully"});
+     } catch (error) {
+         return wrapResponse(error.statusCode, {message: error.message});
+     }
 };
