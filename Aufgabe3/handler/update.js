47cbdf5348d781135904c1f5aaeb8a33cf05cc84
christianmaierger <c.maier1122@gmail.com>
2022-01-18 19:27:34 +0100

    all single ressource methods tested and annotated/changed, todo better validation in update to control if user gave any changed input

--- Aufgabe3/handler/update.js
+++ Aufgabe3/handler/update.js
@@ -35,23 +35,59 @@ async function updateItem(itemChanges, item) {
         throw errorType.notAllNecessaryInformation;
     }
 
+    console.log(itemChanges)
+    console.log(item)
+
+    /** let noChanges=true;
+     for (const key in itemChanges) {
+        const value = itemChanges[key];
+        if (value != item[key] && (key != "birthday" && key != "password" && key != "email")) {
+            console.log("value ist: " + value + " value von item ist " + item[key] + "key sind " + key)
+            if (typeof value == "object" && typeof item[key] == "object") {
+                console.log("both objects")
+                for (var key2 in value) {
+                    console.log(value[key2])
+
+                    let val = value[key2];
+                    let val2= item[key]
+                    console.log(val2[key2])
+                    if (val != val2[key2]) {
+                        console.log(val + val2[key2] )
+                        noChanges=false;
+                    }
+                }
+            }
+            if (typeof value != "object") {
+                console.log("different and no obj")
+                noChanges=false;
+            }
+        }
+    }
+     console.log(noChanges)
+     **/
     let updateItemBundle = {};
     updateItemBundle.email = item.email;
     updateItemBundle.birthday = item.birthday;
+
+    // validation würde ich vorziehen, können ja aufhören, wenn Unsinn in der Änderung ist? und pw würde ich da mit machen
+    // der Konsistenz wegen
+
+    await validateItem(itemChanges);
+
 
     //todo email nicht änderbar?
     delete itemChanges.email;
     delete itemChanges.birthday;
     delete itemChanges.token;
 
-    if (Object.keys(itemChanges).length === 0) {
-        return;
+
+    // added response to let user know nothing was changed, because only bday/mail/token given or the same attributes
+    if (Object.keys(itemChanges).length === 0 /**|| noChanges == true**/) {
+        return wrapResponse(400, {message: 'No changed values given - No update made'});
     }
 
-    // validation würde ich vorziehen, können ja aufhören, wenn Unsin ni nder Änderung ist? und pw würde ich da mit machen
-    // der Konsistenz wegen
-    await validateItem(itemChanges);
 
+    // important do not use name as key, update expression will fail, for example use fullname but also not full-name
     for (const key in itemChanges) {
         const value = itemChanges[key];
         if (key === "password") {
@@ -59,21 +95,16 @@ async function updateItem(itemChanges, item) {
             itemChanges[key] = hashPassword(value);
         }
         updateItemBundle[key] = value;
+        console.log(key + "----" + value)
     }
-
-     /*if (itemChanges["password"]) {
-         validatePassword(value);
-         itemChanges[key] = hashPassword(value);
-         updateItemBundle= itemChanges;
-     }*/
 
     updateItemBundle.prio = createPriority(item.birthday, item.system_relevance, item.pre_diseases);
 
-    //await validateItem(updateItemBundle);
 
     const params = wrapUpdateParams(updateItemBundle);
     try {
         await docClient.update(params).promise();
+        return wrapResponse(200, {message: 'Entry updated successfully'});
     } catch (error) {
         throw errorType.dberror;
     }
@@ -82,11 +113,18 @@ async function updateItem(itemChanges, item) {
 module.exports.update = async (event) => {
     try {
         const item = JSON.parse(event.requestContext.authorizer.item);
-        const itemChanges = JSON.parse(event.body).item;
 
-        await updateItem(itemChanges, item);
-        return wrapResponse(200, {message: 'Entry updated successfully'});
+        let itemChanges;
+        if (event.body) {
+            itemChanges = JSON.parse(event.body).item;
+        } else {
+            return wrapResponse(400, {message: 'No changed values given - No update made'});
+        }
+
+        let response = await updateItem(itemChanges, item);
+        return wrapResponse(response.statusCode, JSON.parse(response.body));
     } catch (err) {
         return handleError(err);
     }
 };
+
