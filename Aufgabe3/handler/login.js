9fcdfeac70dad1dca2aa919f237b272df09a9f36
Lukas <lukas@nitzl.de>
2022-01-19 16:45:53 +0100

    Fix authorization

--- Aufgabe3/handler/login.js
+++ Aufgabe3/handler/login.js
@@ -1,6 +1,7 @@
 const {wrapResponse} = require("../shared");
 
 module.exports.login = async (event) => {
+    console.log(event);
     const token = event.requestContext.authorizer.token;
     return wrapResponse(200, {
         message: "Login successful.",
